5b7b35c73f622d2ee653078b4a8003c52ea7ac4a
praneeth <lakshmipraneeth1461@gmail.com>
2025-02-02 22:14:13 +0530

    add

--- src/main/java/com/example/portfolio/repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.portfolio.repository;
-
-import com.example.portfolio.entity.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<Stock, Long> {
-}
--- src/main/java/com/example/portfolio/util/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.portfolio.util;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Constants {
-    public static final List<String> AVAILABLE_TICKERS = new ArrayList<>(List.of("AAPL", "MSFT", "GOOGL", "AMZN", "TSLA"));
-}
